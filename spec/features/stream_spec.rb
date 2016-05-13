9ea8b0385d1f215a47ce4f80cc234f6041942689
Djiffit <djiffit@gmail.com>
2016-05-13 20:54:52 +0300

    test testing fifth test testing

--- spec/features/stream_spec.rb
+++ spec/features/stream_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 require 'helpers'
 
 describe "opening streams" do
-   it "twitch works" do
-     visit '/twitch/lirik'
-     expect(page).to have_css('iframe', src="http://player.twitch.tv/?channel=lirik")
-   end
+  it "twitch works" do
+    visit '/twitch/lirik'
+    expect(page).to have_css("#lirik")
+  end
 
   it "ustream works" do
     visit '/ustream/nasa'
-    expect(page).to have_css('iframe')
+    expect(page).to have_css('#sidebar')
   end
 
   it "azubu works" do
     visit '/azubu/EPGTV'
-    expect(page).to have_css('iframe')
+    expect(page).to have_css('#EPGTV')
   end
 
   it "youtube works" do
     visit '/youtube/wyK7YuwUWsU'
-    expect(page).to have_css('iframe')
+    expect(page).to have_css('#wyK7YuwUWsU')
   end
 end
