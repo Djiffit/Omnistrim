1bc2dfd3669fc7ebadc1de82e6c5bc987bb0c212
Djiffit <djiffit@gmail.com>
2016-05-13 20:41:05 +0300

    testing test testing

--- spec/features/stream_spec.rb
+++ spec/features/stream_spec.rb
@@ -5,18 +5,20 @@ describe "opening streams" do
    it "twitch works" do
      visit '/twitch/lirik'
      expect(page).to have_css('iframe', src="http://player.twitch.tv/?channel=lirik")
-     save_and_open_page
    end
 
   it "ustream works" do
-
+    visit '/ustream/nasa'
+    expect(page).to have_css('iframe')
   end
 
   it "azubu works" do
-
+    visit '/azubu/EPGTV'
+    expect(page).to have_css('iframe')
   end
 
   it "youtube works" do
-
+    visit '/youtube/wyK7YuwUWsU'
+    expect(page).to have_css('iframe')
   end
 end
