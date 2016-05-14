be6f0137d1ab260d1b68ede476925e934c63ac49
Djiffit <djiffit@gmail.com>
2016-05-14 16:36:21 +0300

    pls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@
 RSpec.configure do |config|
   require 'coveralls'
   Coveralls.wear!
-ENV["RAILS_ENV"] = 'development'
+ENV["RAILS_ENV"] = 'test'
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
